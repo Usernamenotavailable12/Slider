39ccd63c9271bc42b58da4e5505a296f0c68a6e9
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-05-20 12:22:56 +0400

    Update sliderScript-takeshape.js

--- sliderScript-takeshape.js
+++ sliderScript-takeshape.js
@@ -29,7 +29,7 @@
       body: JSON.stringify({
         query: `
           query GetSlides($locale: String!) {
-            getSlideList(locale: $locale) {
+            getSlideList(locale: $locale, size: 30) {
               items {
                 caption
                 pages
